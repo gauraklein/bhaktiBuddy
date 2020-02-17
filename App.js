34596d5442cb869a51ade8218778e7c1e87a4797
gauraklein <gaurakklein@gmail.com>
2020-02-17 16:31:49 -0600

    basic file structure setup, working on adding redux

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Display} from './src/display'
 
-export default function App() {
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    
+      <Display />
+   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View } from 'react-native';
+
+export function Display () {
+
+    return (
+        <View>
+            <Text>TESTING THE DISPLAY</Text>
+        </View>
+    )
+
+}
