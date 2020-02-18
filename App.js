d5ab837c01095b9e664a93b3f056a1e682c4443c
gauraklein <gaurakklein@gmail.com>
2020-02-18 13:05:17 -0600

    added in react navigator, now just need to create my routes

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
+import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Button, Text, View } from "react-native";
 import Display from "./src/display";
-import { Provider} from "react-redux";
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
-import reducers from './src/redux/rootReducer'
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./src/redux/rootReducer";
 
-const reducer = combineReducers(reducers)
+const reducer = combineReducers(reducers);
 
 const store = createStore(reducer, applyMiddleware(thunk));
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   // console.log(store, "this is the store")
   return (
     <Provider store={store}>
-    <Display />
+      {/* <Display> */}
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Display} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      {/* </Display> */}
     </Provider>
-  )
+  );
 }
-
