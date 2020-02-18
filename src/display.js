bcb9c5ad80ee7158c5e951879254ce33ecfdedc4
gauraklein <gaurakklein@gmail.com>
2020-02-18 10:18:41 -0600

    updated display container to be full screen

--- src/display.js
+++ src/display.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Stylesheet, Text, View } from "react-native";
+import { Stylesheet, Text, View, Dimensions } from "react-native";
 
 export function Display(props) {
     console.log(props)
+    const screenWidth = Dimensions.get('window').width
+    const screenHeight = Dimensions.get('window').height
   return (
-    <View>
-      <Text>TESTING THE DISPLAY</Text>
+    <View style={{width: screenWidth, height: screenHeight}}>
+      <Text style={{marginTop: 100}}>TESTING THE DISPLAY</Text>
     </View>
   );
 }
