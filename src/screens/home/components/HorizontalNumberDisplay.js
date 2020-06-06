5be95de7bce7f9b168bb02caea8ccc6094843ac4
gauraklein <gaurakklein@gmail.com>
2020-06-06 15:13:13 -0500

    working on adding in horizontal stat displayer:

---
+++ src/screens/home/components/HorizontalNumberDisplay.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import {homeStyles} from '../homeStylesheet';
+import { useSelector } from 'react-redux';
+import { globalStyles } from '../../../globalStylesheet';
+
+const HorizontalNumberDisplay = () => {
+    return (
+        <>
+            <Text>HORIZONTAL DISPLAY</Text>
+        </>
+    )
+}
+
+export default HorizontalNumberDisplay;
